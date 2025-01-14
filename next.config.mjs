9f2741548fa3c9dff7b850a1590592f8294b3f55
Akhilesh Jadhav <akhileshjadhav26@gmail.com>
2025-01-14 18:57:37 -0500

    Update Next.js and GitHub Actions config for static export

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/TechDocs' : '',
+  images: {
+    unoptimized: true,
+  },
   eslint: {
     ignoreDuringBuilds: true
   },
   typescript: {
     ignoreBuildErrors: true
-  }
+  },
+  trailingSlash: true,
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/TechDocs/' : ''
 };
 
 export default nextConfig;
